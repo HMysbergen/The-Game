01595c1a2534a968e0f0c46e46b5804934a68cdc
HugoMysbergen <99039461@mydavinci.nl>
2018-10-15 15:03:04 +0200

    Ik ben opnieuw begonnen, ik vind dit nog steeds een moeilijke opdracht kan iemand mij helpen?

---
+++ Thegame.js
@@ -0,0 +1,10 @@
+var leven	= 3;
+var links 	= -1;
+var rechts 	= 0;
+
+alert("Je loopt in een bos... het is erg donker, je ziet een groot leeg gebouw staan en je besluit er naartoe te gaan.")
+	alert("Er zijn twee deuren, op de linker deur staat: betreden op eigen risico. Op de rechter deur staat: gevaarlijke stoffen aanwezig.")
+		var deur1 	=prompt("Welke deur kies je links/rechts?")
+			if(deur1 == rechts) {
+				
+			}
